6f360278de82ca4df1099df6f943a7645db2358d
bob pasker <bob@pasker.net>
2019-10-21 15:38:34 -0400

    added web socket client + server

--- src/App.js
+++ src/App.js
@@ -23,4 +23,30 @@ function App() {
   );
 }
 
+
+function showMessage(message) {
+    console.log(message);
+//    messages.textContent += `\n${message}`;
+//    messages.scrollTop = messages.scrollHeight;
+}
+
+const currentRoute= 'localhost:8080' // window.location.host
+console.log(currentRoute);
+const ws = new WebSocket(`ws://${currentRoute}`);
+ws.onerror = function(event, source, lineno, colno, error ) {
+    var msg = `ERROR: ${JSON.stringify(event)}`;
+    showMessage(msg);
+};
+ws.onopen = function() {
+    showMessage('OPEN');
+};
+ws.onclose = function() {
+    showMessage('CLOSE');
+};
+ws.onmessage = function(message) {
+    showMessage(`MESSAGE ${message.data} `);
+
+}
+
+
 export default App;
---
+++ src/console.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const uuid = require('uuid');
+
+const WebSocket = require('ws');
+
+const app = express();
+const map = new Map();
+
+//
+// Serve static files from the 'public' folder.
+//
+app.use(express.static('public'));
+
+//
+// Create HTTP server by ourselves.
+//
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function(ws, request) {
+    
+    const id = setInterval(function() {
+	console.log("SEND tick");
+	ws.send("tick");
+    }, 1000);
+
+    
+    ws.on('message', function(message) {
+	//
+	// Here we can now use session parameters.
+	//
+	console.log(`MESSAGE ${message} `);
+	const response = `echo "${message}"`;
+	ws.send(response);
+	console.log(`SEND "${response}"`);
+
+    });
+    
+    ws.on('close', function() {
+	clearInterval(id)
+	console.log(`CLOSE`);
+    });
+
+    ws.on('error', function(errorEvent) {
+	console.log(`ERROR ${JSON.stringify(errorEvent)}`);
+    })
+
+
+})
+
+
+       
+// Start the server.
+//
+server.listen(8080, function() {
+    console.log('Listening on http://localhost:8080');
+});
+
