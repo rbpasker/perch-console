4823fd44775a574d260e48b38273ffdbb6ee4dc8
bob pasker <bob@pasker.net>
2019-10-28 10:29:12 -0400

    test program now has 2 producers

--- src/xray_test.js
+++ src/xray_test.js
@@ -1,20 +1,19 @@
 const  {XRayProducer,XRayConsumer} = require('./xray');
 
-cons = new XRayConsumer("localhost");
+// two producers
+prod1 = new XRayProducer("test", {"test0":"", "test1":""} );
+prod2 = new XRayProducer("foo", {"foo":"", "bar":""} );
 
-prod = new XRayProducer("test", {"test0":"", "test1":""} );
-console.log(prod);
-prod = new XRayProducer("foo", {"foo":"", "bar":""} );
-
+// one consumer
 cons2 = new XRayConsumer("localhost");
 
-console.log(prod);
-
-
-function again() { 
-    prod.send({"test1":"abc"});
-    setTimeout(again, 1000);
+// 
+function again(prod) { 
+    console.log(`test: sending to ${prod.id}`);
+    prod.send({"key":`message for ${prod.id}`});
+    setTimeout(again, 1000, prod);
 }    
 
-again();
+again(prod1);
+again(prod2);
 
